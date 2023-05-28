ca3c5553b683ba4ea5cf2c39a01e02bf646ddd78
OHind <ouchani.hind@gmail.com>
2023-05-28 09:59:49 -0700

    Creating first empty files

---
+++ README.md
@@ -0,0 +1,1 @@
+A reproduction of the standard printf function
---
+++ printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _printf - print
+ * @format: the format to print
+ * Return: the number of caracter printed
+ */
+int _printf(const char *format, ...)
+{
+	return (0);
+}
