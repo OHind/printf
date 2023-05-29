85837420d877e9b46637e43cfcbb8be0929487bb
“taissir-l” <taissirlassoued1@gmail.com>
2023-05-29 14:13:47 -0700

    printf_main_fonction

--- _printf.c
+++ _printf.c
@@ -1,7 +1,9 @@
 #include "main.h"
 #include< stdarg.h>
 
+
 void print_buffer(char buffer[], int *buff_ind);
+
 
 /**
  * _printf - Printf function replica.
@@ -12,16 +14,14 @@ void print_buffer(char buffer[], int *buff_ind);
 
 int _printf(const char *format, ...)
 {
-	int i, to_be_printed = 0, print_chars = 0;
+	int i, p = 0, print_chars = 0;
 	int flags, width, precision, size, buff_i = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
-
 	va_start(list, format);
-
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
@@ -39,18 +39,14 @@ int _printf(const char *format, ...)
 			precision = find_precision(format, &i, list);
 			size = find_size(format, &i);
 			++i;
-			to_be_printed = type_to_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (to_be_printed == -1)
+			p = type_to_print(format, &i, list, buffer, flags, width, precision, size);
+			if (p == -1)
 				return (-1);
-			print_chars += to_be_printed;
+			print_chars += p;
 		}
 	}
-
 	print_buffer(buffer, &buff_ind);
-
 	va_end(list);
-
 	return (print_chars);
 }
 
@@ -64,10 +60,6 @@ int _printf(const char *format, ...)
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
-		for (int i = 0; i < *buff_ind; i++)
-		{
-			_putchar(buffer[i]);
-		}
-
+		write(1, &buffer[0], *buff_ind);
 	*buff_ind = 0;
 }
